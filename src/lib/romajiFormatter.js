f9b278f8d0e24312689e8b26271027699aff88df
Gary Vincent <gtvincent2000@gmail.com>
2025-08-21 17:29:35 -0500

    Fix dictPath scope and resolve kuromoji dict correctly

--- src/lib/romajiFormatter.js
+++ src/lib/romajiFormatter.js
@@ -1,14 +1,18 @@
+import path from "path";
+import { fileURLToPath } from "url";
 import kuromoji from "kuromoji";
 import * as wanakana from "wanakana";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const dictPath = path.resolve(__dirname, "../lib/kuromoji_dict");
 
 export async function formatRomajiWithTokenizer(sentence) {
   return new Promise((resolve, reject) => {
-    kuromoji.builder({ dicPath: "src/lib/kuromoji_dict" }).build((err, tokenizer) => {
+    kuromoji.builder({ dicPath: dictPath }).build((err, tokenizer) => {
       if (err) return reject(err);
 
       const tokens = tokenizer.tokenize(sentence);
-
       const kanaWords = [];
       const romajiWords = [];
 
@@ -30,4 +34,3 @@ export async function formatRomajiWithTokenizer(sentence) {
     });
   });
 }
-
